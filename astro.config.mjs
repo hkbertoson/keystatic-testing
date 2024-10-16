8049ad5e7ddfc6d1f8f25096785b7f4360dc51e3
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-10-16 14:38:45 -0500

    Changed Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
-import cloudflare from '@astrojs/cloudflare';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
   output: 'hybrid',
-  adapter: cloudflare(),
+  adapter: vercel(),
 });
