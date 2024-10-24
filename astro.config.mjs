9eed6eeba399cf21ea8fb7d8f8f77bc32433d4d0
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-10-24 15:05:38 -0500

    Updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
-import netlify from '@astrojs/netlify';
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
-  output: 'hybrid',
-  adapter: netlify(),
+  output: 'server',
+  adapter: cloudflare(),
 });
