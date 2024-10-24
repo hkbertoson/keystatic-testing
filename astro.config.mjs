5b5ef3646b6ad38ed7786b5ee07935314ff53b8f
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-10-24 15:12:51 -0500

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
 
-import cloudflare from '@astrojs/cloudflare'
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
-  vite: {
-    ssr: {
-      external: ['node:path','node:fs','node:fs/promises']
-    },
-  },
-  output: 'server',
-  adapter: cloudflare(),
+	integrations: [react(), markdoc(), keystatic()],
+	vite: {
+		ssr: {
+			external: ["node:path", "node:fs", "node:fs/promises"],
+		},
+	},
+	output: "server",
+	adapter: cloudflare(),
 });
